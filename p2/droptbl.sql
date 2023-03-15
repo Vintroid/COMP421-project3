320cb9424cae4ce489fd2d74b605577149d09261
Vincent St-Pierre <vstpierre1@gmail.com>
2023-03-14 21:34:25 -0400

    added p2 folder with files from project 2

---
+++ p2/droptbl.sql
@@ -0,0 +1,32 @@
+-- Include your drop table DDL statements in this file.
+
+-- Make sure to terminate each statement with a semicolon (;)
+
+-- LEAVE this statement on. It is required to connect to your database.
+CONNECT TO cs421;
+
+-- Remember to put the drop table ddls for the tables with foreign key references
+--    ONLY AFTER the parent tables has already been dropped (reverse of the creation order).
+
+-- This is only an example of how you add drop table ddls to this file.
+--   You may remove it.
+drop table Clients;
+drop table Stadiums;
+drop table Seats;
+drop table Teams;
+drop table Associations;
+drop table Supervises;
+drop table Members;
+drop table Coaches;
+drop table Players;
+drop table Belongs;
+drop table Matches;
+drop table Referees;
+drop table Oversees;
+drop table Hosts;
+drop table Tickets;
+drop table Bookings;
+drop table Plays;
+drop table Goals;
+drop table Scores;
+drop table Happens;
